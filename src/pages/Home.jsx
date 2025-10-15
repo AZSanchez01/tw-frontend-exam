e7a02b029fea6505256f898d786cb34404a5db00
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-10-15 14:44:51 +0800

    Feat: Create Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,43 @@ import { ArticlesContext } from "../App";
 import LeadArticle from "../components/LeadArticle";
 import ArticleCardSmallList from "../components/ArticleCardSmallList";
 import ArticleCard from "../components/ArticleCard";
+import styles from "./styles/Home.module.css";
 
 function Home() {
   const { articles } = useContext(ArticlesContext);
   return (
     <>
-      <div className={`site-side-padding`}>
+      <section className={`site-side-padding ${styles.section}`}>
         <LeadArticle {...articles.sports[0]} />
-      </div>
-      <br />
-      <br />
-      <div className={`site-side-padding`}>
-        <ArticleCardSmallList heading="Reports" articles={articles.reports} />
-      </div>
-      <br />
-      <div className={`site-side-padding`}>
-        <ArticleCardSmallList heading="Blogs" articles={articles.blogs} />
-      </div>
-      <br />
-      <div className={`site-side-padding`}>
-        <ArticleCardSmallList heading="Sports" articles={articles.sports} />
-      </div>
-      <br />
-      <br />
-      <section className={`site-side-padding`}>
-        <ArticleCard {...articles.sports[0]} />
       </section>
-      <br />
-      <br />
+      <section className={`site-side-padding ${styles.section}`}>
+        <ArticleCardSmallList
+          heading="Latest"
+          articles={[
+            articles.sports[1],
+            articles.sports[2],
+            articles.sports[3],
+            articles.blogs[0],
+          ]}
+        />
+      </section>
+      <section
+        className={`site-side-padding color-bg-dark ${styles.section} ${styles.featured}`}
+      >
+        <ArticleCard {...articles.reports[1]} color={`white`} />
+        <ArticleCard {...articles.reports[3]} color={`white`} />
+      </section>
+      <section className={`site-side-padding ${styles.section}`}>
+        <ArticleCardSmallList
+          heading="More Articles"
+          articles={[
+            articles.reports[0],
+            articles.blogs[1],
+            articles.blogs[2],
+            articles.blogs[3],
+          ]}
+        />
+      </section>
     </>
   );
 }
